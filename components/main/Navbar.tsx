21dc5f58b96ed19282189433acb685f0d0257cc5
Sandun Tharuka <sandun.suntws@gmail.com>
2024-04-23 20:05:49 +0530

    change style in navbar

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
         <a
           href="#about-me"
-          className="h-auto w-auto flex flex-row items-center mr-5 "
+          className="h-auto md:w-auto flex flex-row items-center mr-5 "
         >
 
           <Image
@@ -25,7 +25,7 @@ const Navbar = () => {
           </span>
         </a>
 
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
+        <div className="hidden w-[500px] h-full md:flex flex-row items-center justify-between md:mr-20">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
             <a href="#about-me" className="cursor-pointer">
               About me
